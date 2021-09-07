bc68c21dfda870fc796f4622401bf2fdd178d102
Elisha Hung <mrhchief@gmail.com>
2021-09-07 16:46:25 +0800

    add cors

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is web root.
-        </p>
-      </header>
-    </div>
-  );
+
+const API_URL = process.env.node_env === 'production' ? '/api' : 'http://pipeline.moonshinevfx.com/api';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            guestbookData: ''
+        }
+    }
+
+    async fetchGuestBook() {
+        const response = await fetch(
+            `${API_URL}/sql`
+        );
+        const data = await response.json();
+        this.setState(state => state.guestbookData = JSON.stringify(data));
+    }
+
+    render() {
+      return (
+          <div className="App">
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <button onClick={this.fetchGuestBook}>
+                      Click to Show
+                  </button>
+                  <p>
+                      This is web root.
+                  </p>
+                  <p>{this.state.guestbookData}</p>
+              </header>
+          </div>
+      );
+    }
 }
 
 export default App;
