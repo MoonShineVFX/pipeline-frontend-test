d462d6c864baaaa95cbcd406fb8e0a6bd367304b
Elisha Hung <mrhchief@gmail.com>
2021-09-07 13:36:22 +0800

    Initial commit

---
+++ admin/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://storage.googleapis.com/public-lib/product.jpg' className="App-logo" alt="logo" />
+        <p>
+          Here is admin control center yo.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
